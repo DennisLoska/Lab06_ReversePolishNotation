7a409ee0314a9160a09edfb33746c3e16697f9d7
DennisTalmid <d.loska@hotmail.de>
2017-05-16 17:08:53 +0200

    adding relevant comments

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -4,8 +4,10 @@ public interface Stack<E> {
 
 	public boolean isEmpty();
 
+	//return this
 	public E top() throws Underflow;
 
+	//return this
 	public Stack<E> push(E element);
 
 	public Stack<E> pop() throws Underflow;
